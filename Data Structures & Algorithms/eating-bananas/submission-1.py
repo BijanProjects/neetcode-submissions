fa001b79649ce796eae0512bd97cf2427f6cbce2
Bijan <42376152+BijanProjects@users.noreply.github.com>
2026-07-30 17:32:33 -0230

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left_k = 1
+        right_k = max(piles)
+        k = right_k
+        while left_k <= right_k:
+            middle_k = (left_k + right_k) // 2
+            hours = self.timeline(piles, middle_k)
+            if hours <= h:
+                k = min(k, middle_k)
+                right_k = middle_k - 1
+            elif hours > h:
+                left_k = middle_k + 1
+        return k
+
+
+
+
+
+    def timeline(self, piles, k):
+        hours = 0
+        for bananas in piles:
+            if bananas % k == 0:
+                hours += bananas // k
+            else:
+                hours += bananas // k + 1
+        return hours 
